55083d360a26cf4754255945240e326aa86d615d
Arnav Arora <arnava2004@gmail.com>
2023-10-19 01:16:12 -0400

    added functions

--- Sudoku and Wave Function Collaps/mainSudoku.py
+++ Sudoku and Wave Function Collaps/mainSudoku.py
@@ -23,16 +23,37 @@ def solveBoard(boardCheck):
         row, col = emptyPoint
     
     for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        if valid(boardCheck, i, (row, col)):
+            boardCheck[row][col] = i
 
-            if solve(bo):
+            if solveBoard(boardCheck):
                 return True
 
-            bo[row][col] = 0
+            boardCheck[row][col] = 0
 
     return False
     
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
 
 def print_board(bo):
     for i in range(len(bo)):
